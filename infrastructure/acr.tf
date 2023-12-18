9eb1beccc8a38fefeb94e8115e0cf67a761a7ffe
ransaked1 <danbraghis@yahoo.com>
2023-12-18 17:31:53 +0000

    added task to purge web images

--- infrastructure/acr.tf
+++ infrastructure/acr.tf
@@ -8,8 +8,8 @@ resource "azurerm_container_registry" "kissa_acr" {
   admin_enabled       = true
 }
 
-resource "azurerm_container_registry_task" "kissa_task" {
-  name                = "purgetask"
+resource "azurerm_container_registry_task" "kissa_task_api" {
+  name                = "purgetask_api"
   container_registry_id = azurerm_container_registry.kissa_acr.id
 
   platform {
@@ -34,8 +34,8 @@ resource "azurerm_container_registry_task" "kissa_task" {
   }
 }
 
-resource "azurerm_container_registry_task" "kissa_task_dev" {
-  name                = "purgetask_dev"
+resource "azurerm_container_registry_task" "kissa_task__api_dev" {
+  name                = "purgetask_api_dev"
   container_registry_id = azurerm_container_registry.kissa_acr.id
 
   platform {
@@ -47,6 +47,32 @@ resource "azurerm_container_registry_task" "kissa_task_dev" {
     version: v1.1.0
     steps:
       - cmd: acr purge --filter 'REPO:kissa-api-dev.*' --ago 1h
+        disableWorkingDirectoryOverride: true
+        timeout: 3600
+    EOF
+    context_path = "/dev/null"
+  }
+
+  timer_trigger {
+    name     = "t1"
+    schedule = "0 0 * * *" // Deletes every hour
+    enabled  = true
+  }
+}
+
+resource "azurerm_container_registry_task" "kissa_task_web" {
+  name                = "purgetask_web"
+  container_registry_id = azurerm_container_registry.kissa_acr.id
+
+  platform {
+    os = "Linux"
+  }
+
+  encoded_step {
+    task_content = <<EOF
+    version: v1.1.0
+    steps:
+      - cmd: acr purge --filter 'REPO:kissa-web.*' --ago 1h
         disableWorkingDirectoryOverride: true
         timeout: 3600
     EOF
