ef475a47d2763dabccd134e3fd24fe00c65cdad2
ransaked1 <danbraghis@yahoo.com>
2023-12-18 13:50:16 +0000

    updated clean tasks

--- infrastructure/acr.tf
+++ infrastructure/acr.tf
@@ -20,7 +20,33 @@ resource "azurerm_container_registry_task" "kissa_task" {
     task_content = <<EOF
     version: v1.1.0
     steps:
-      - cmd: acr purge --filter 'REPO:kissa_api.*' --ago 1h
+      - cmd: acr purge --filter 'REPO:kissa-api.*' --ago 1h
+        disableWorkingDirectoryOverride: true
+        timeout: 3600
+    EOF
+    context_path = "/dev/null"
+  }
+
+  timer_trigger {
+    name     = "t1"
+    schedule = "0 */12 * * *"
+    enabled  = true
+  }
+}
+
+resource "azurerm_container_registry_task" "kissa_task_dev" {
+  name                = "purgetask_dev"
+  container_registry_id = azurerm_container_registry.kissa_acr.id
+
+  platform {
+    os = "Linux"
+  }
+
+  encoded_step {
+    task_content = <<EOF
+    version: v1.1.0
+    steps:
+      - cmd: acr purge --filter 'REPO:kissa-api-dev.*' --ago 1h
         disableWorkingDirectoryOverride: true
         timeout: 3600
     EOF
