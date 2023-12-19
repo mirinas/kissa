f55806f698cac53a0a8263cd450e4bbdfa474994
ransaked1 <danbraghis@yahoo.com>
2023-12-19 00:51:15 +0000

    CORS and request

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,23 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  const API_ENDPOINT = process.env.KISSA_API_ENDPOINT || 'http://localhost:8080';
+  console.log(API_ENDPOINT);
+
+  const handleClick = async () => {
+    try {
+      const response = await fetch(`${API_ENDPOINT}/`);
+      const data = await response.json();
+      setMessage(data.message);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +25,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload. Deploy tested.
         </p>
+        <button onClick={handleClick}>Fetch API</button>
+        <p>{message}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
