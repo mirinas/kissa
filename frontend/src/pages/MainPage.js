36ca39479a6aa7843dc2f481fe7761ca5485856e
am26g21 <am26g21@soton.ac.uk>
2024-01-02 17:25:10 +0200

    Changed API url variable

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -2,10 +2,17 @@ import {useOutletContext} from "react-router-dom";
 import '../styles/MainPage.css'
 import cat from '../media/cat.avif'
 import {useEffect} from "react";
+import {API_ENDPOINT} from "../globals";
+import axios from 'axios'
 
 export default function MainPage() {
     const {setSelected} = useOutletContext();
-    useEffect(() => setSelected('main'));
+    useEffect(() => setSelected('main'), []);
+
+    const handleMatch = () => {
+        console.log(API_ENDPOINT);
+        axios.get(API_ENDPOINT + '/profile'
+    }
 
     return (
         <div className={'profile-view'}>
@@ -14,7 +21,7 @@ export default function MainPage() {
                 <h3>Haika <span>1.5km</span></h3>
                 <p>Description about the owner and the cat</p>
                 <div className={'match-options'}>
-                    <button>Match</button>
+                    <button onClick={handleMatch}>Match</button>
                     <button>Skip</button>
                 </div>
             </div>
