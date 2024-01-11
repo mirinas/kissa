1655dceaeb27c032fce94a0a70a14f4f162b057d
am26g21 <am26g21@soton.ac.uk>
2024-01-11 04:27:08 +0200

    Registration validation message fix

--- frontend/src/pages/ProfileRegistrationForm.js
+++ frontend/src/pages/ProfileRegistrationForm.js
@@ -98,7 +98,7 @@ function UserForm({ setState })
 
         if (password !== passwordConfirm) {
             console.error("Passwords do not match");
-            setErrorMessage("Passwords do not match");
+            setError("Passwords do not match");
             return;
         }
         fetch(API_ENDPOINT + "/profiles/register", {
@@ -175,7 +175,7 @@ function UserForm({ setState })
         })
         .catch((err) => {
             console.error('Registration Error:', err);
-            setErrorMessage(err.message);
+            setError(err.message);
         });
     };
 
@@ -344,7 +344,7 @@ function UserForm({ setState })
                     <button className="btn_kissa" onClick={handleRegister} disabled={disabled}>Next</button>
                 </div>
 
-                <p className='smalltext'>{errorMessage}</p>
+                <p className='smalltext'>{error}</p>
 
             </div>
         </form>
